466f0c21d729ab966b70671dd8ae4ee68ae1014b
GowthamAnbu <gautamgow@gmail.com>
2017-03-04 08:55:57 +0530

    working module updated

--- Tag-Core/src/test/java/com/tag/dao/TestUserDAO.java
+++ Tag-Core/src/test/java/com/tag/dao/TestUserDAO.java
@@ -1,18 +1,20 @@
 package com.tag.dao;
 
-import com.tag.dao.UserDAO;
+import com.google.gson.Gson;
 
 public class TestUserDAO {
 
 	public static void main(String[] args) {
 		UserDAO userDAO = new UserDAO();
-		if(userDAO.isValidPassword("gautamgow@gmail.com","Gowtham")){
+		/*if(userDAO.isValidPassword("gautamgow@gmail.com","Gowtham")){
 			System.out.println("yes");
 		}
 		else{
 			System.out.println("No");
-		}
-
+		}*/
+		System.out.println(userDAO.findAll());
+		String json=new Gson().toJson( userDAO.findAll());
+		System.out.println(json);
 	}
 
 }
