eaf698292c4aed42abc41a848df40c142d2aaa53
GowthamAnbu <gautamgow@gmail.com>
2017-03-05 10:27:17 +0530

    registered user completed

--- Tag-Core/src/main/java/com/tag/dao/ComplaintDAO.java
+++ Tag-Core/src/main/java/com/tag/dao/ComplaintDAO.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.tag.model.Complaint;
 import com.tag.model.Department;
+import com.tag.model.Status;
 import com.tag.model.User;
 import com.tag.util.ConnectionUtil;
 
@@ -70,6 +71,68 @@ public class ComplaintDAO {
 		String sql = "SELECT USER_ID FROM COMPLAINTS WHERE ID=?";
 		Object[] args = { complaintId };
 		return jdbcTemplate.queryForObject(sql,args,int.class);
+	}
+	
+	public List<Complaint> findbyUserId(Integer userId) {
+		String sql = "SELECT ID,NAME,USER_ID,DEPARTMENT_ID,DOOR_NUMBER,STREET_NAME,PINCODE,DETAILS FROM COMPLAINTS WHERE USER_ID=? AND STATUS_ID=1";
+		Object[] args={userId};
+		return jdbcTemplate.query(sql, args, (rs, rowNum) -> {
+			final Complaint complaint = new Complaint();
+			final User user = new User();
+			user.setId(rs.getInt("USER_ID"));
+			complaint.setId(rs.getInt("ID"));
+			complaint.setUser(user);
+			complaint.setName(rs.getString("NAME"));
+			Department department = new Department();
+			department.setId(rs.getInt("DEPARTMENT_ID"));
+			complaint.setDepartment(department);
+			complaint.setDoorNo(rs.getString("DOOR_NUMBER"));
+			complaint.setStreetName(rs.getString("STREET_NAME"));
+			complaint.setPincode(rs.getString("PINCODE"));
+			complaint.setDetails(rs.getString("DETAILS"));
+			return complaint;
+		});
+	}
+	
+	public List<Complaint> getEmployee(Integer userId) {
+		String sql = "SELECT C.ID AS COMPLAINT_ID,C.NAME AS COMPLAINT_NAME,CE.`EMPLOYEE_ID` EMPLOYEE_ID,C.`STATUS_ID`AS STATUS_ID FROM COMPLAINTS C LEFT JOIN COMPLAINTS_EMPLOYEE CE ON C.`ID`=CE.`COMPLAINT_ID`WHERE C.`STATUS_ID` IN (3,4) AND C.`USER_ID`=?";
+		Object[] args = { userId };
+		return jdbcTemplate.query(sql, args, (rs, rowNum) -> {
+			final Complaint complaint = new Complaint();
+			complaint.setId(rs.getInt("COMPLAINT_ID"));
+			complaint.setName(rs.getString("COMPLAINT_NAME"));
+			final User user = new User();
+			user.setId(rs.getInt("EMPLOYEE_ID"));
+			final Status status = new Status();
+			status.setId(rs.getInt("STATUS_ID"));
+			complaint.setStatus(status);
+			complaint.setUser(user);
+			return complaint;
+		});
+	}
+	
+	public List<Complaint> viewComplaintStatus(Integer userId) {
+		String sql = "SELECT ID,NAME,USER_ID,DEPARTMENT_ID,DOOR_NUMBER,STREET_NAME,PINCODE,DETAILS,STATUS_ID FROM COMPLAINTS WHERE USER_ID=?";
+		Object[] args={userId};
+		return jdbcTemplate.query(sql, args, (rs, rowNum) -> {
+			final Complaint complaint = new Complaint();
+			final User user = new User();
+			user.setId(rs.getInt("USER_ID"));
+			complaint.setId(rs.getInt("ID"));
+			complaint.setUser(user);
+			complaint.setName(rs.getString("NAME"));
+			Department department = new Department();
+			department.setId(rs.getInt("DEPARTMENT_ID"));
+			complaint.setDepartment(department);
+			complaint.setDoorNo(rs.getString("DOOR_NUMBER"));
+			complaint.setStreetName(rs.getString("STREET_NAME"));
+			complaint.setPincode(rs.getString("PINCODE"));
+			complaint.setDetails(rs.getString("DETAILS"));
+			final Status status = new Status();
+			status.setId(rs.getInt("STATUS_ID"));
+			complaint.setStatus(status);
+			return complaint;
+		});
 	}
 	
 }
--- Tag-Core/src/main/java/com/tag/dao/EmployeeDetailDAO.java
+++ Tag-Core/src/main/java/com/tag/dao/EmployeeDetailDAO.java
@@ -6,17 +6,19 @@ import com.tag.model.Employee;
 import com.tag.util.ConnectionUtil;
 
 public class EmployeeDetailDAO {
-	private JdbcTemplate jdbcTemplate=ConnectionUtil.getJdbcTemplate();
-	
-	public void save(Employee employee){
-		String sql="INSERT INTO EMPLOYEE_DETAILS (ID,DEPARTMENT_ID,DESIGNATION_ID) VALUES(?,?,?);";
-		Object[] args={employee.getUser().getId(),employee.getDepartment().getId(),employee.getDesignation().getId()};
-		jdbcTemplate.update(sql,args);
+	private JdbcTemplate jdbcTemplate = ConnectionUtil.getJdbcTemplate();
+
+	public void save(Employee employee) {
+		String sql = "INSERT INTO EMPLOYEE_DETAILS (ID,DEPARTMENT_ID,DESIGNATION_ID) VALUES(?,?,?);";
+		Object[] args = { employee.getUser().getId(), employee.getDepartment().getId(),
+				employee.getDesignation().getId() };
+		jdbcTemplate.update(sql, args);
 	}
-	
-	public int getDesignation(Integer employeeId){
-		String sql="SELECT IFNULL((SELECT DESIGNATION_ID FROM EMPLOYEE_DETAILS WHERE ID=?),0)";
-		Object[] args={employeeId};
-		return jdbcTemplate.queryForObject(sql,args,int.class);
+
+	public int getDesignation(Integer employeeId) {
+		String sql = "SELECT IFNULL((SELECT DESIGNATION_ID FROM EMPLOYEE_DETAILS WHERE ID=?),0)";
+		Object[] args = { employeeId };
+		return jdbcTemplate.queryForObject(sql, args, int.class);
 	}
+
 }
